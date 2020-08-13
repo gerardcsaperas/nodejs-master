5c3671b4750072a94fcfbd679c68f19865d3e648
Gerard C. Saperas <gcsaperas@gmail.com>
2020-08-13 22:56:07 +0200

    partial commit nodejs first project

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
+// Import dependencies
+const validatr = require('validator');
+
 // Imports function from another file
 const getNotes = require('./notes.js');
 
 console.log(getNotes());
+
+console.log(validator.isEmail('gerarrard.com'));
