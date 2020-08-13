b05358a68c6648849ebec9c1d250071fa6dcf04a
Gerard C. Saperas <gcsaperas@gmail.com>
2020-08-13 20:13:58 +0200

    requires

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-// Require File System Module
-const fs = require('fs');
+// Imports function from another file
+const getNotes = require('./notes.js');
 
-// If the text file doesn't exist, it will be created. If it already exists, it will be overwritten.
-fs.writeFileSync('notes.txt', 'This file was created by Node.js!');
-
-// Challenge: Append a message to notes.txt
-//
-// 1. Use appendFileSync to append to the file
-// 2. Run the script
-// 3. Check your work by opening the file and viewing the appended text
-fs.appendFileSync('notes.txt', '\n\nChallenge accepted...(actually, completed)');
+console.log(getNotes());
---
+++ notes.js
@@ -0,0 +1,4 @@
+const getNotes = () => 'Your notes...';
+
+// Exports previously declared function in order to use in other files
+module.exports = getNotes;
